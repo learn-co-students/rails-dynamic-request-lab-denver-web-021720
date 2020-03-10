12af5be6bd956849bb05d1ddaaf07314ed9f48b7
mmrozewski <mmrozews@gmail.com>
2020-03-10 17:08:39 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render json: @students
   end
+
+  def show
+    @student = Student.find(params[:id])
+
+  end
+
 end
